65a0659486d112a6fa57d5cbfe94c8d355b82131
unknown <arjunansibichakravarthy007@gmail.com>
2025-04-29 08:52:22 +0530

    update package.json

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ app.use('/api/admin', adminRoutes);
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+ 
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
