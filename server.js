14fe4700290fc45a0d026bceda75f2d5656884af
unknown <arjunansibichakravarthy007@gmail.com>
2025-05-21 10:42:03 +0530

    update the message

--- server.js
+++ server.js
@@ -33,5 +33,6 @@ mongoose.connect(process.env.MONGO_URI, {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`👍 Server started successfully on port ${PORT}. Ready to go the backend work properly! 😊`);
 });
+
